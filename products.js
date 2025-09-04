94637a5280572b40c5a040f2c75988fa95201666
Anandhu-CHN <anandhu27122005@gmail.com>
2025-09-04 20:12:00 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,80 @@
+
+
+
+
+const products=[ 
+{
+productName:'Football',
+imageName:'Images/Footballstar.png',
+Id:'e1',
+pricePaise:89950 
+},
+{
+productName:'Samsung Galaxy Smart Phone',
+imageName:'Images/Phone.jpg',
+Id:'e2',
+pricePaise:2000000 
+},
+{
+productName:'Ponds Branded face cream',
+imageName:'Images/FaceCream.jpg',
+Id:'e3',
+pricePaise:9990 
+},
+{
+productName:'HP Laptop',
+imageName:'',
+Id:'e4',
+pricePaise:4679900 
+},
+{
+productName :
+'Lenovo Laptop'
+,
+imageName:'',
+Id:'e5',
+pricePaise:4679900 
+},
+{
+productName:'Enchanting Perfume',
+imageName:'',
+Id:'e6',
+pricePaise:6600 
+},
+{
+productName:'Boots and Football Combo',
+imageName:'',
+Id:'e7',
+pricePaise:365900 
+},
+{
+productName:'Rubik‘s Cube',
+imageName:'',
+Id:'e8',
+pricePaise:15700
+},
+{
+productName:'Biryani Mix',
+imageName:'',
+Id:'e9',
+pricePaise:25700
+},
+{
+productName:'Vegetables kit',
+imageName:'',
+Id:'e10',
+pricePaise:14700
+},
+{
+productName:'Make Up Kit',
+imageName:'',
+Id:'e11',
+pricePaise:49750
+},
+{
+productName:'Gaming Accessories',
+imageName:'',
+Id:'e12',
+pricePaise:570000
+}
+];
