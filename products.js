40aa22e46f0c96adb186d5b73f1881d4e4948713
Anandhu-CHN <anandhu27122005@gmail.com>
2025-09-04 21:08:11 +0530

    Update products.js

--- products.js
+++ products.js
@@ -4,8 +4,8 @@
 
  const products = [
   { productName: 'Exciting Games Collection', imageName: 'Images/ Games.jpg', Id: 'e1', pricePaise: 29900, productQuantity: 1 },
-  { productName: 'Premium Ball and Boot Combo', imageName: 'Images/BallAndBoot.jpg', Id: 'e2', pricePaise: 44900, productQuantity: 1 },
-  { productName: 'Durable Cricket Bat', imageName: 'Images/Bat.jpg', Id: 'e3', pricePaise: 19900, productQuantity: 1 },
+  { productName: 'Premium Ball and Boot Combo', imageName: 'Images/BallAndBoot.jpg', Id: 'e2', pricePaise: 114900, productQuantity: 1 },
+  { productName: 'Durable Cricket Bat', imageName: 'Images/Bat.jpg', Id: 'e3', pricePaise: 69900, productQuantity: 1 },
   { productName: 'Delicious Chocolate Pack', imageName: 'Images/Chocolate.jpg', Id: 'e4', pricePaise: 4900, productQuantity: 1 },
   { productName: 'Luxury Cosmetics Set', imageName: 'Images/Cosmetics.jpg', Id: 'e5', pricePaise: 59900, productQuantity: 1 },
   { productName: 'Classic Dishes Set', imageName: 'Images/Dishes.jpg', Id: 'e6', pricePaise: 24900, productQuantity: 1 },
@@ -15,15 +15,15 @@
   { productName: 'Creamy Ice Cream Tub', imageName: 'Images/IceCream.jpg', Id: 'e10', pricePaise: 6000, productQuantity: 1 },
   { productName: 'Stylish Winter Jacket', imageName: 'Images/Jacket.jpg', Id: 'e11', pricePaise: 99900, productQuantity: 1 },
   { productName: 'Elegant Necklace', imageName: 'Images/Necklace.jpg', Id: 'e12', pricePaise: 79900, productQuantity: 1 },
-  { productName: 'Non-Stick Pan', imageName: 'Images/Pan.jpg', Id: 'e13', pricePaise: 19900, productQuantity: 1 },
+  { productName: 'Non-Stick Pan', imageName: 'Images/Pan.jpg', Id: 'e13', pricePaise: 39900, productQuantity: 1 },
   { productName: 'Refreshing Perfume', imageName: 'Images/Perfume.jpg', Id: 'e14', pricePaise: 39900, productQuantity: 1 },
   { productName: 'Smartphone 2025 Edition', imageName: 'Images/Phone.jpg', Id: 'e15', pricePaise: 999900, productQuantity: 1 },
   { productName: 'Casual Cotton Shirt', imageName: 'Images/Shirt.jpg', Id: 'e16', pricePaise: 39900, productQuantity: 1 },
-  { productName: 'Shirt and Pant Combo', imageName: 'Images/ShirtAndPant.jpg', Id: 'e17', pricePaise: 59900, productQuantity: 1 },
+  { productName: 'Shirt and Pant Combo', imageName: 'Images/ShirtAndPant.jpg', Id: 'e17', pricePaise: 89900, productQuantity: 1 },
   { productName: 'Portable Bluetooth Speaker', imageName: 'Images/Speaker.jpg', Id: 'e18', pricePaise: 49900, productQuantity: 1 },
-  { productName: 'LED Torch Light', imageName: 'Images/Torch.jpg', Id: 'e19', pricePaise: 9900, productQuantity: 1 },
+  { productName: 'LED Torch Light', imageName: 'Images/Torch.jpg', Id: 'e19', pricePaise: 15900, productQuantity: 1 },
   { productName: 'Ultra HD Smart TV', imageName: 'Images/Tv.jpg', Id: 'e20', pricePaise: 1499900, productQuantity: 1 },
   { productName: 'Fresh Organic Vegetables', imageName: 'Images/Vegetables.jpg', Id: 'e21', pricePaise: 3900, productQuantity: 1 },
-  { productName: 'Luxury Wrist Watch', imageName: 'Images/Watch.jpg', Id: 'e22', pricePaise: 59900, productQuantity: 1 },
+  { productName: 'Luxury Wrist Watch', imageName: 'Images/Watch.jpg', Id: 'e22', pricePaise: 179900, productQuantity: 1 },
   { productName: 'Aquarelle Art Poster', imageName: 'Images/top-view-colorful-aquarelle-with-copy-space.jpg', Id: 'e23', pricePaise: 12900, productQuantity: 1 }
 ];
